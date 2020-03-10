bc3e80d7ed45738080dda71bd11010f95b92e976
pangeon <kamil.cecherz@gmail.com>
2020-03-10 20:59:49 +0100

    Pierwsze próby z widokiem aplikacji

---
+++ src/main/java/pl/cecherz/calcbill/controller/web/OwnerController.java
@@ -0,0 +1,168 @@
+package pl.cecherz.calcbill.controller.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import pl.cecherz.calcbill.exeptions.EmptyFindResultException;
+import pl.cecherz.calcbill.exeptions.EntityEmptyContentException;
+import pl.cecherz.calcbill.exeptions.EntityNotFoundException;
+import pl.cecherz.calcbill.exeptions.RestExceptionHandler;
+import pl.cecherz.calcbill.model.db.Owner;
+import pl.cecherz.calcbill.model.db.Payments;
+import pl.cecherz.calcbill.repositories.OwnerRepository;
+import pl.cecherz.calcbill.utils.MessageBuilder;
+import pl.cecherz.calcbill.utils.PaymentsCalculator;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Klasa ma zastosowanie w przypadku odwoływania się do całego zbioru płatności z uwzględnienieniem
+ * podziału na posiadaczy. Każdy obiekt Owners zawiera w sobie listę danych płatności, które tworzone są by wykorzystaniu
+ * klasy PaymentsController. Relacje występują na poziomie dazy danych.
+ *
+ * Metody
+ * --------
+ * getAllOwners()
+ * getOwner(/{id})
+ * getOwnerPayments(/{id}/payments)
+ * getOwnerPaymentsByKind(/{id}/payments/{kind})
+ * getOwnerPaymentsByAmonutRange(/{id}/payments/{min}/{max})
+ * getSumOwnerPayments(/{id}/payments/sum)
+ * getSumOwnerPaymentsByKind(/{id}/payments/sum/{kind})
+ *
+ * addOwner()
+ * updateOwner({id})
+ * replaceOwner({id})
+ * deleteOwner({id}) - usuwa dane z bazy: tabele Owner i Payments
+ *
+ */
+@RestController
+@RequestMapping("/api/web/owners")
+@Component("OwnerControllerWeb")
+public class OwnerController extends RestExceptionHandler {
+    private MessageBuilder message = new MessageBuilder(OwnerController.class);
+    private final OwnerRepository ownerRepository;
+
+    public OwnerController(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+    @GetMapping()
+    public ModelAndView getAllOwners() {
+        ModelAndView ownersListView = new ModelAndView("owners_list");
+        ownersListView.addObject("owners", ownerRepository.findAll());
+        return ownersListView;
+    }
+    @GetMapping("/{id}")
+    public Owner getOwner(
+            @PathVariable Integer id) {
+        Owner owner = ownerRepository.findOwnerById(id);
+        if (owner == null) throw new EntityNotFoundException(id);
+        message.getInfo("getOwner()", owner, "status", HttpStatus.OK);
+        return owner;
+    }
+    @GetMapping("/{id}/payments")
+    public List<Payments> getOwnerPayments(
+            @PathVariable Integer id) {
+        Owner owner = ownerRepository.findOwnerById(id);
+        if (owner == null) throw new EntityNotFoundException(id);
+
+        List<Payments> ownerPayments = owner.getPayments();
+        if (ownerPayments.isEmpty()) throw new EntityEmptyContentException(id);
+
+        message.getInfo("getOwnerPayments()", ownerPayments);
+        return ownerPayments;
+    }
+    @GetMapping("/{id}/payments/{kind}")
+    public List<Payments> getOwnerPaymentsByKind(
+            @PathVariable Integer id,
+            @PathVariable String kind) {
+        Owner owner = ownerRepository.findOwnerById(id);
+        if (owner == null) throw new EntityNotFoundException(id);
+
+        final List<Payments> ownerPaymentsByKind = owner.getPayments()
+                .stream().filter(payment -> payment.getKind().equals(kind))
+                .collect(Collectors.toList());
+        if (ownerPaymentsByKind.isEmpty()) throw new EmptyFindResultException(id, kind);
+
+        message.getInfo("getOwnerPaymentsByKind()", ownerPaymentsByKind);
+        return ownerPaymentsByKind;
+    }
+    @GetMapping("/{id}/payments/{min}/{max}")
+    public List<Payments> getOwnerPaymentsByAmonutRange(
+            @PathVariable Integer id,
+            @PathVariable Double min,
+            @PathVariable Double max) {
+        Owner owner = ownerRepository.findOwnerById(id);
+        if (owner == null) throw new EntityNotFoundException(id);
+
+        final List<Payments> ownerPaymentsByAmountRange = ownerRepository.findOwnerById(id).getPayments()
+                .stream().filter(payment -> payment.getAmount() > min && payment.getAmount() < max)
+                .collect(Collectors.toList());
+        String range = min + " - " + max;
+        if(ownerPaymentsByAmountRange.isEmpty()) throw new EmptyFindResultException(id, range);
+        message.getInfo("getOwnerPaymentsByAmonutRange()", ownerPaymentsByAmountRange);
+        return ownerPaymentsByAmountRange;
+    }
+    @GetMapping(value = "/{id}/payments/sum", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Double getSumOwnerPayments(
+            @PathVariable Integer id) {
+        Double sum = PaymentsCalculator.sumOwnerPayments(id, ownerRepository);
+        message.getInfo("getSumOwnerPayments()", sum, "ownerId", id);
+        return sum;
+    }
+    @GetMapping(value = "/{id}/payments/sum/{kind}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Double getSumOwnerPaymentsByKind(
+            @PathVariable Integer id,
+            @PathVariable String kind) {
+        Double sum = PaymentsCalculator.sumOwnerPaymentsByKind(id, kind, ownerRepository);
+        message.getInfo("getSumOwnerPaymentsByKind()", sum, "ownerId", id, "kind", kind);
+        return sum;
+    }
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addOwner(@RequestBody Owner body) {
+        message.getInfo("addOwner()", body);
+        ownerRepository.save(body);
+    }
+    @PutMapping("/{id}")
+    public void replaceOwner(
+            @PathVariable Integer id,
+            @RequestBody Owner newOwner) {
+        Optional<Owner> ownerValuesToReplace = ownerRepository.findById(id);
+        if (ownerValuesToReplace.orElse(null) == null) throw new EntityNotFoundException(id);
+        message.getInfo("start :: replaceOwner()", ownerValuesToReplace);
+        ownerValuesToReplace.ifPresent(owner -> {
+            owner.setId(id);
+            owner.setName(newOwner.getName());
+            owner.setSurname(newOwner.getSurname());
+        });
+        ownerRepository.save(ownerValuesToReplace.orElse(null));
+        message.getInfo("end :: replaceOwner()", newOwner);
+    }
+    @PatchMapping("/{id}")
+    public void updateOwner(
+            @PathVariable Integer id,
+            @RequestBody Owner ownerToUpdate) {
+        Optional<Owner> ownerValuesToUpdate = ownerRepository.findById(id);
+        if (ownerValuesToUpdate.orElse(null) == null) throw new EntityNotFoundException(id);
+        message.getInfo("start :: updateOwner()", ownerValuesToUpdate);
+        ownerValuesToUpdate.ifPresent(owner -> {
+            if(ownerToUpdate.getId() != null) owner.setId(id);
+            if(ownerToUpdate.getName() != null) owner.setName(ownerToUpdate.getName());
+            if(ownerToUpdate.getSurname() != null) owner.setSurname(ownerToUpdate.getSurname());
+        });
+        ownerRepository.save(ownerValuesToUpdate.orElse(null));
+        message.getInfo("end :: updateOwner()", ownerValuesToUpdate);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteOwner(@PathVariable Integer id) {
+        Owner ownerToDelete = ownerRepository.findOwnerById(id);
+        if(ownerToDelete == null) throw new EntityNotFoundException(id);
+        message.getInfo("deleteOwner()", ownerToDelete);
+        ownerRepository.delete(ownerToDelete);
+    }
+}
---
+++ src/main/java/pl/cecherz/calcbill/controller/web/PaymentsController.java
@@ -0,0 +1,131 @@
+package pl.cecherz.calcbill.controller.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+import pl.cecherz.calcbill.exeptions.DataIntegrityException;
+import pl.cecherz.calcbill.exeptions.EmptyFindResultException;
+import pl.cecherz.calcbill.exeptions.EntityNotFoundException;
+import pl.cecherz.calcbill.exeptions.RestExceptionHandler;
+import pl.cecherz.calcbill.model.db.Owner;
+import pl.cecherz.calcbill.model.db.Payments;
+import pl.cecherz.calcbill.repositories.PaymentsRepository;
+import pl.cecherz.calcbill.utils.MessageBuilder;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Klasa ma zastosowanie w przypadku odwoływania się do całego zbioru płatność bez uwzględnienia podziału na posiadaczy.
+ *
+ * Metody
+ * --------
+ * getAllPayments()
+ * getPayment({id})
+ * filterPaymentsByKind(? kind)
+ * filterPaymentsByOwnerIdAndAmonutRang(? min max)
+ *
+ * addPayment({id}) -- tworzy relacje na poziomie bazy danych
+ * replacePayment({id})
+ * updatePayment({id})
+ * deletePayment({id})
+ */
+
+@RestController
+@RequestMapping("/api/web/payments")
+@Component("PaymentsControllerWeb")
+public class PaymentsController extends RestExceptionHandler {
+    private MessageBuilder message = new MessageBuilder(PaymentsController.class);
+    private final PaymentsRepository paymentsRepository;
+
+    public PaymentsController(PaymentsRepository paymentsRepository) {
+        this.paymentsRepository = paymentsRepository;
+    }
+    @GetMapping()
+    public Iterable<Payments> getAllPayments() {
+        message.getInfo("getAllPayments()", paymentsRepository.findAll());
+        return paymentsRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public Payments getPayment(
+            @PathVariable Integer id) {
+        Payments payment = paymentsRepository.findPaymentById(id);
+        if (payment == null) throw new EntityNotFoundException(id);
+        message.getInfo("getPayment()", payment);
+        return payment;
+    }
+    @GetMapping(params = "kind")
+    public List<Payments> filterPaymentsByKind(
+            @RequestParam("kind") String kind) {
+        List<Payments> paymentsByKind = paymentsRepository.findPaymentsByKind(kind);
+        message.getInfo("filterPaymentsByKind()", paymentsByKind);
+
+        if (paymentsByKind.isEmpty()) throw new EmptyFindResultException(kind);
+        return paymentsByKind;
+    }
+    @GetMapping(params = {"min", "max"})
+    public List<Payments> filterPaymentsByOwnerIdAndAmonutRange(
+            @RequestParam("min") Double min,
+            @RequestParam("max") Double max) {
+        List<Payments> paymentsByAmount = paymentsRepository.findPaymentsByAmountBetween(min, max);
+        message.getInfo("filterPaymentsByOwnerIdAndAmonutRange()", paymentsByAmount);
+
+        String range = min + " - " + max;
+        if (paymentsByAmount.isEmpty()) throw new EmptyFindResultException(range);
+        return paymentsByAmount;
+    }
+    @PostMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addPayment (
+            @RequestBody Payments body,
+            @PathVariable Integer id) {
+        Owner owner = new Owner();
+        owner.setId(id);
+        body.setOwnerId(owner);
+        body.setDate(new Timestamp(System.currentTimeMillis()));
+        message.getInfo("addPayment()", body, owner);
+        try {
+            paymentsRepository.save(body);
+        } catch (Exception e) {
+            throw new DataIntegrityException(id, e.getCause().toString(), e.getMessage());
+        }
+    }
+    @PutMapping("/{id}")
+    public void replacePayment(@PathVariable Integer id, @RequestBody Payments newPayment) {
+        Optional<Payments> paymentsValuesToReplace = paymentsRepository.findById(id);
+        if(paymentsValuesToReplace.orElse(null) == null) throw new EntityNotFoundException(id);
+        message.getInfo("start :: replacePayments()", paymentsValuesToReplace);
+        paymentsValuesToReplace.ifPresent(payment -> {
+            payment.setId(id);
+            payment.setAmount(newPayment.getAmount());
+            payment.setKind(newPayment.getKind());
+            payment.setDate(new Timestamp(System.currentTimeMillis()));
+        });
+        paymentsRepository.save(paymentsValuesToReplace.orElse(null));
+        message.getInfo("end :: replacePayments()", newPayment);
+    }
+    @PatchMapping("/{id}")
+    public void updatePayment(
+            @PathVariable Integer id,
+            @RequestBody Payments paymentToUpdate) {
+        Optional<Payments> paymentValuesToUpdate = paymentsRepository.findById(id);
+        if(paymentValuesToUpdate.orElse(null) == null) throw new EntityNotFoundException(id);
+        message.getInfo("start :: updatePayment()", paymentValuesToUpdate);
+        paymentValuesToUpdate.ifPresent(payment -> {
+            if(paymentToUpdate.getId() != null) payment.setId(id);
+            if(paymentToUpdate.getAmount() != null) payment.setAmount(paymentToUpdate.getAmount());
+            if(paymentToUpdate.getKind() != null) payment.setKind(paymentToUpdate.getKind());
+            payment.setDate(new Timestamp(System.currentTimeMillis()));
+        });
+        paymentsRepository.save(paymentValuesToUpdate.orElse(null));
+        message.getInfo("end :: updatePayment()", paymentValuesToUpdate);
+    }
+    @DeleteMapping("/{id}")
+    void deletePayment(@PathVariable Integer id) {
+        Payments paymentToDelete = paymentsRepository.findPaymentById(id);
+        if(paymentToDelete == null) throw new EntityNotFoundException(id);
+        message.getInfo("deletePayment()", paymentToDelete);
+        paymentsRepository.delete(paymentToDelete);
+    }
+}
