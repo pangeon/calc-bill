4ecec427009fe72732a9251a0ac847f2e0fc535d
pangeon <kamil.cecherz@gmail.com>
2020-03-14 13:15:04 +0100

    Praca nad widokiem płatności dla użytkownika

--- src/main/resources/static/js/owner_payments_list.js
+++ src/main/resources/static/js/owner_payments_list.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
-
-    var id = document.getElementById("id").value;
-    alert(id);
-
-    $.ajax({
-        url: "http://localhost:8080/api/db/owners/"+ id +"/payments"
-    }).then(function(data) {
-        for (var i = 0; data.length; i++) {
-            $('.owner-payments-list')
-                .append(
-                    '<tr>' +
-                    '<td>' + data[i].id + '</td>' +
-                    '<td>' + data[i].amount + '</td>' +
-                    '<td>' + data[i].kind + '</td>' +
-                    '<td>' + data[i].date + '</td>' +
-                    '<td><a href="/">Edytuj</a></td>' +
-                    '<td><a href="/">Usuń</a></td>' +
-                    '</tr>')
-        }
+    $("#button").click(function (event) {
+            event.preventDefault();
+            var ids = $('#id').val();
+            $.ajax({
+                url: "http://localhost:8080/api/db/owners/"+ ids +"/payments"
+            }).then(function(data) {
+                for (var i = 0; data.length; i++) {
+                    $('.owner-payments-list')
+                        .append(
+                            '<tr>' +
+                            '<td>' + data[i].id + '</td>' +
+                            '<td>' + data[i].amount + '</td>' +
+                            '<td>' + data[i].kind + '</td>' +
+                            '<td>' + data[i].date + '</td>' +
+                            '<td><a href="/">Edytuj</a></td>' +
+                            '<td><a href="/">Usuń</a></td>' +
+                            '</tr>')
+                }
+            });
+            $("#button").prop("disabled", true);
     });
 });
+
+
